1138fdbfd2d6797f54c3880f9a76dc1128d4772c
Sujal391 <modisujal31@gmail.com>
2025-12-25 16:42:01 +0530

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-semibold">
+        My Next.js App
+      </h1>
+    </main>
+  );
+}
